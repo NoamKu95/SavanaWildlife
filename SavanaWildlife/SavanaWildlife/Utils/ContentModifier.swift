452d68a6e2bc822ba7dd021187e91555fbb2fcb0
Noam Kurtzer <noamkurtzer@gmail.com>
2023-03-05 00:10:17 +0200

    Added Stickers

---
+++ SavanaWildlife/SavanaWildlife/Utils/ContentModifier.swift
@@ -0,0 +1,8 @@
+//
+//  ContentModifier.swift
+//  SavanaWildlife
+//
+//  Created by Noam Kurtzer on 05/03/2023.
+//
+
+import Foundation
---
+++ SavanaWildlife/SavanaWildlife/Views/CopyrightView.swift
@@ -0,0 +1,20 @@
+//
+//  CopyrightView.swift
+//  SavanaWildlife
+//
+//  Created by Noam Kurtzer on 04/03/2023.
+//
+
+import SwiftUI
+
+struct CopyrightView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CopyrightView_Previews: PreviewProvider {
+    static var previews: some View {
+        CopyrightView()
+    }
+}

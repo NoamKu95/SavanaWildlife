2ae3d672e29927209ee9d83887387d75a56fd0c8
Noam Kurtzer <noamkurtzer@gmail.com>
2023-03-04 18:59:17 +0200

    Added content to the Gallery screen - images, grid, slider

--- SavanaWildlife/SavanaWildlife/Screens/Gallery.swift
+++ SavanaWildlife/SavanaWildlife/Screens/Gallery.swift
@@ -8,9 +8,60 @@
 import SwiftUI
 
 struct Gallery: View {
+    
+    let animals: [Animal] = Bundle.main.decode("animals.json")
+    @State private var selectedAnimal: String = "lion"
+    
+    @State private var gridLayout: [GridItem] = [GridItem(.flexible())]
+    @State private var columns: Double = 3.0
+    
+    let heticFeedback = UIImpactFeedbackGenerator(style: .medium)
+    
+    func gridSwitch() {
+        gridLayout = Array(repeating: GridItem(.flexible()), count: Int(columns))
+    }
+
+    
     var body: some View {
         ScrollView (.vertical, showsIndicators: false) {
-            Text("Gallery")
+            
+            VStack (alignment: .center, spacing: 30) {
+                
+                // BIG IMAGE
+                Image(selectedAnimal)
+                    .resizable()
+                    .scaledToFit()
+                    .clipShape(Circle())
+                    .overlay(Circle().stroke(Color.white, lineWidth: 8))
+                
+                // SLIDER
+                Slider(value: $columns, in: 2...4, step: 1)
+                    .padding(.horizontal)
+                    .onChange(of: columns, perform: { _ in
+                        gridSwitch()
+                    })
+                
+                // GRID
+                LazyVGrid (columns: gridLayout, alignment: .center, spacing: 10) {
+                    ForEach(animals) { item in
+                        Image(item.imageName)
+                            .resizable()
+                            .scaledToFit()
+                            .clipShape(Circle())
+                            .overlay(Circle().stroke(Color.white, lineWidth: 1))
+                            .onTapGesture {
+                                selectedAnimal = item.imageName
+                                heticFeedback.impactOccurred()
+                            }
+                    }
+                }
+                .animation(.easeIn)
+                .onAppear(perform: {
+                    gridSwitch()
+                })
+            }
+            .padding(.horizontal)
+            .padding(.vertical, 15)
         }
         .frame(maxWidth: .infinity, maxHeight: .infinity)
         .background(MotionAnimationView())

04e882a94010e1e82421a2f4bc2afa7264117663
Noam Kurtzer <noamkurtzer@gmail.com>
2023-03-04 13:49:52 +0200

    Added a coordinates indication panel on the map

--- SavanaWildlife/SavanaWildlife/Screens/MapScreen.swift
+++ SavanaWildlife/SavanaWildlife/Screens/MapScreen.swift
@@ -21,12 +21,50 @@ struct MapScreen: View {
     let locations: [AnimalLocation] = Bundle.main.decode("locations.json")
     
     var body: some View {
-        Map(coordinateRegion: $region,
-            annotationItems: locations) { item in
+        Map(coordinateRegion: $region, annotationItems: locations) { item in
             MapAnnotation(coordinate: item.location) {
                 MapAnnotationView(location: item)
             }
         }
+        .overlay(
+            HStack (alignment: .center, spacing: 12) {
+                Image("compass")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 48, height: 48)
+                
+                VStack (alignment: .leading, spacing: 3) {
+                    HStack {
+                        Text("Latitude: ")
+                            .font(.footnote)
+                            .fontWeight(.bold)
+                            .foregroundColor(.accentColor)
+                        Spacer()
+                        Text("\(region.center.latitude)")
+                            .font(.footnote)
+                            .foregroundColor(.white)
+                    }
+                    HStack {
+                        Text("Longitude: ")
+                            .font(.footnote)
+                            .fontWeight(.bold)
+                            .foregroundColor(.accentColor)
+                        Spacer()
+                        Text("\(region.center.longitude)")
+                            .font(.footnote)
+                            .foregroundColor(.white)
+                    }
+                }
+            }
+                .padding(.vertical, 12)
+                .padding(.horizontal, 16)
+                .background(Color.black
+                    .cornerRadius(8)
+                    .opacity(0.6)
+                )
+                .padding()
+            , alignment: .top
+        )
     }
 }
 
